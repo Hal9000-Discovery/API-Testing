00ab7db270c02e8d5028a82a87417e63ce85fc83
David Sanghara <davesanghara@gmail.com>
2025-07-22 07:44:42 -0700

    updated comments

--- main_renamed.py
+++ main_renamed.py
@@ -1,3 +1,9 @@
+# main_renamed.py
+# This file is part of the REST API project.
+# It is a simple Flask application that serves as the main entry point.
+# It has been renamed from 'main.py' to 'main_renamed.py' to avoid conflicts.
+
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
